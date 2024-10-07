9e1134234cc8837bc20154ceb65613002ba4d03e
Kiril Yakymchuk <stbestich@gmail.com>
2024-10-07 19:28:50 +0300

    created SeaBoat class

---
+++ SeaBoat.java
@@ -0,0 +1,15 @@
+public class SeaBoat {
+  public final String name;
+  public final String type;
+  public final double length;
+  public final double width;
+  public final int passangersAmount;
+
+  public SeaBoat(String name, String type, double length, double width, int passangersAmount) {
+    this.name = name;
+    this.type = type;
+    this.length = length;
+    this.width = width;
+    this.passangersAmount = passangersAmount;
+  }
+}
